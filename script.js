671933bca0669534eba15cb11df07e6675199155
Dafne Hernandez <mcflurrycerezita@gmail.com>
2025-11-19 14:28:27 -0600

    feat: parseFloat implementation for transform strings

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Convert numbers from String to Number (float)
+function convertToNumber(a,b){
+    return [parseFloat(a), parseFloat(b)];
+}
+
+//Validate that both numbers aren't equal to zero
 function validateNumber(a,b){
     if (a == 0 && b == 0){
         return 0;
@@ -6,11 +12,13 @@ function validateNumber(a,b){
 
 //Create function addition
 function addition(a,b){
+    [a,b] = convertToNumber(a,b);
     validateNumber(a,b);
     return a + b;
 }
 //Create function subtraction
 function subtraction(a,b){
+    [a,b] = convertToNumber(a,b);
     validateNumber(a,b);
     if (b == 0){
         return a;
@@ -20,6 +28,7 @@ function subtraction(a,b){
 }
 //Create function multiplication
 function multiplication(a,b){
+    [a,b] = convertToNumber(a,b);
     validateNumber(a,b);
     if(a == 0  || b == 0 ){
         return 0;
@@ -31,6 +40,7 @@ function multiplication(a,b){
 }
 //Create function division
 function division(a,b){
+    [a,b] = convertToNumber(a,b);
     validateNumber(a,b);
     if (b == 0){
         return 0;
@@ -43,10 +53,19 @@ function division(a,b){
     }   
 }
 
-let result = addition(10,5)
+/*let result = addition(10,5)
 let result2 = subtraction(4,5)
 let result3 = multiplication(6,8)
 let result4 = division(4,2)
 
-console.log("El restulado de la suma es: " + result, ", el resultado de resta es: " + result2,
+console.log("El resultado de la suma es: " + result, ", el resultado de resta es: " + result2,
 ", el resultado de la multiplicacion es: " + result3, ", el resultado de la division es: "+ result4);
+
+*/
+
+// Usando strings para los numeros
+let num1 = "10";
+let num2 = "5";
+let result = addition(num1, num2);
+
+console.log("Resultado de la operacion es: "+ result);
